b4d31ce8e818c3ee81e2a0df3b5818da31364e28
aksaber <498550830@qq.com>
2020-07-21 19:06:22 +0800

    flutter init

---
+++ android/app/src/main/kotlin/com/example/hvideo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hvideo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  // 强制横屏
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  // Widget build(BuildContext context) {
+  //   // final wordPair = new WordPair.random();
+  //   return new MaterialApp(
+  //       title: 'Welcome to Flutter',
+  //       home: new Scaffold(
+  //           appBar: new AppBar(
+  //             title: new Text('Welcome to Flutter'),
+  //           ),
+  //           body: new Center(
+  //             // child: new Text(wordPair.asPascalCase),
+  //             child: new RandomWords(),
+  //           )));
+  // }
+
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Startup Name Generator',
+      home: new RandomWords(),
+    );
+  }
+}
+
+// 添加一个有状态的部件（Stateful widget）
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+// 添加一个基本的build方法，该方法通过将生成单词对的代码从MyApp移动到RandomWordsState来生成单词对
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = new WordPair.random();
+    // return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          // 在每一列之前，添加一个1像素高的分割线widget
+          if (i.isOdd) return new Divider();
+
+          // 语法 i ~/ 2 表示 i除以2 ，但返回值是整形（向下取整）
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+        title: new Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: new Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        });
+  }
+}
